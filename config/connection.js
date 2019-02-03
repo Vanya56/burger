4771ce85aa461d48e1dcdd1fe00decb08346abcc
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2019-02-03 00:12:40 -0700

    get and put routes fixed

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
+    port: 8889,
     host: 'localhost',
     user: 'root',
     password: 'root',
--- config/orm.js
+++ config/orm.js
@@ -7,6 +7,14 @@ var orm = {
             if(err) throw err;
             cb(result)
         })
+    },
+    update: function(tableInput, condition, cb) {
+        connection.query('UPDATE ' +tableInput+' SET devoured=true WHERE id='
+        +condition+';', function(err, result) {
+        if(err)throw err;
+        cb(result);
+
+        })
     }
 }
 module.exports = orm;
